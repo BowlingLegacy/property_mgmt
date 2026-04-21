b72cca4b7f5853cdb5bb38e2c69f80124448ccd6
BowlingLegacy <bowlinglegacyllc@outlook.com>
2026-04-21 08:05:56 -0700

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('apply/', views.apply, name='apply'),
+    path('application-submitted/', views.application_submitted, name='application_submitted'),
+    path('signup/', views.signup, name='signup'),
 ]
