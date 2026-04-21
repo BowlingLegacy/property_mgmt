d0cbab0c5b75f2ca34b6e809f18deeb9d132e71c
BowlingLegacy <bowlinglegacyllc@outlook.com>
2026-04-21 06:49:59 -0700

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Homepage
     path('', views.home, name='home'),
+
+    # Signup page
+    path('signup/', views.signup, name='signup'),
+]
 ]
