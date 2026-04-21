05767e7cd48437ad4368b153b3f270c15b598562
BowlingLegacy <bowlinglegacyllc@outlook.com>
2026-04-21 06:51:19 -0700

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
+# Homepage
 def home(request):
     return render(request, 'main/home.html')
+
+# Signup page
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # send user to login page after successful signup
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'registration/signup.html', {'form': form})
+
