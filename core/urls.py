f5ebcd59bf0a730966fe36b767ccf496abe1742f
BowlingLegacy <bowlinglegacyllc@outlook.com>
2026-04-21 06:48:43 -0700

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Add this line for login/logout/password reset
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # Your app
     path('', include('main.urls')),
 ]
