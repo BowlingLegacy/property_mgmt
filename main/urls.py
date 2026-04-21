20b3c542b1ccde96ec64f7d82f986ec47ab2600f
BowlingLegacy <bowlinglegacyllc@outlook.com>
2026-04-21 10:40:17 -0700

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('apply/', views.apply, name='apply'),
+    path('', views.property_list, name='home'),
+    path('properties/', views.property_list, name='property_list'),
+    path('properties/<slug:slug>/', views.property_detail, name='property_detail'),
+    path('properties/<slug:slug>/apply/', views.apply_for_property, name='apply_for_property'),
     path('application-submitted/', views.application_submitted, name='application_submitted'),
     path('signup/', views.signup, name='signup'),
 ]
+
