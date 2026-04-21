2977eb4660367e7da171281e531e414ca93d4817
BowlingLegacy <bowlinglegacyllc@outlook.com>
2026-04-20 17:06:12 -0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -7,10 +7,13 @@ SECRET_KEY = 'replace-this-later'
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    'bowlinglegacy.com',
+    'www.bowlinglegacy.com',
     'property-mgmt-tg4s.onrender.com',
     'localhost',
     '127.0.0.1',
 ]
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
