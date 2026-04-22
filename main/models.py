4f670cb77b6e64002e99ffd364ae193f9cdbbc8d
BowlingLegacy <bowlinglegacyllc@outlook.com>
2026-04-21 23:14:41 -0700

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,1 +1,11 @@
+from django.db import models
+
+class Property(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    photo = models.ImageField(upload_to='property_photos/', blank=True, null=True)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
 
