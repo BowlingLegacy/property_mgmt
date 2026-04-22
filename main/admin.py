7e7f231b28e395f3e795c8961734294808e91ace
BowlingLegacy <bowlinglegacyllc@outlook.com>
2026-04-22 00:16:47 -0700

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Property
+from .models import HousingApplication, Property
 
+admin.site.register(HousingApplication)
 admin.site.register(Property)
-
