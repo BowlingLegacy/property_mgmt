f6a23929c1126426056308a7ffd1998ef95eb38b
BowlingLegacy <bowlinglegacyllc@outlook.com>
2026-04-22 00:12:35 -0700

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import HousingApplicationForm
 
 def home(request):
     return render(request, "home.html")
 
+def apply(request):
+    if request.method == "POST":
+        form = HousingApplicationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("apply_success")
+    else:
+        form = HousingApplicationForm()
+
+    return render(request, "apply.html", {"form": form})
+
+def apply_success(request):
+    return render(request, "apply_success.html")
