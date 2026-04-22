789e5b621295ab01e8ba675dffdd84200505bd26
BowlingLegacy <bowlinglegacyllc@outlook.com>
2026-04-22 11:38:12 -0700

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,19 +1,7 @@
-from django.shortcuts import render, redirect
-from .forms import HousingApplicationForm
+from django.shortcuts import render
+from .models import Property
 
 def home(request):
-    return render(request, "home.html")
+    properties = Property.objects.all()
+    return render(request, 'home.html', {'properties': properties})
 
-def apply(request):
-    if request.method == "POST":
-        form = HousingApplicationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("apply_success")
-    else:
-        form = HousingApplicationForm()
-
-    return render(request, "apply.html", {"form": form})
-
-def apply_success(request):
-    return render(request, "apply_success.html")
