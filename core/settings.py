28855546daa3551ce412fd941542e58155877b1b
BowlingLegacy <bowlinglegacyllc@outlook.com>
2026-04-21 18:39:59 -0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "bowlinglegacy.com",
