f2dee54509d2047e8b6dee3279da9ce920a992ab
BowlingLegacy <bowlinglegacyllc@outlook.com>
2026-04-26 03:52:39 -0700

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,7 @@
-from django.contrib import admin
 from django.urls import path
 from main import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-
     # Public pages
     path("", views.home, name="home"),
     path("creed/", views.creed, name="creed"),
