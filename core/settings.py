0c2d4634c460cd01a055eff538346af85f46876f
BowlingLegacy <bowlinglegacyllc@outlook.com>
2026-04-26 06:57:30 -0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -110,7 +110,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # MEDIA FILES
 # ---------------------------------------------------------
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = "/opt/render/project/src/media"
 # ---------------------------------------------------------
 # logging
 # ---------------------------------------------------------
