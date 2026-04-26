0f913b30c6cab47677dd57c1234d60f266550771
BowlingLegacy <bowlinglegacyllc@outlook.com>
2026-04-26 10:13:05 -0700

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
 ]
 
-# ADD THIS LINE
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve uploaded media files on Render
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
