d05b05565c595a652bb1a46485e68510597dd221
BowlingLegacy <bowlinglegacyllc@outlook.com>
2026-04-26 10:26:07 -0700

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,22 +1,14 @@
 from django.contrib import admin
-from .models import Property, User, HousingApplication
+from .models import Property, PropertyImage
 
 
-@admin.register(Property)
+class PropertyImageInline(admin.TabularInline):
+    model = PropertyImage
+    extra = 3
+
+
 class PropertyAdmin(admin.ModelAdmin):
-    list_display = ("id", "name")
-    search_fields = ("name",)
+    inlines = [PropertyImageInline]
 
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ("id", "username", "email", "role", "invite_code")
-    list_filter = ("role",)
-    search_fields = ("username", "email", "invite_code")
-
-
-@admin.register(HousingApplication)
-class HousingApplicationAdmin(admin.ModelAdmin):
-    list_display = ("id", "full_name", "email", "phone", "monthly_income", "created_at")
-    search_fields = ("full_name", "email", "phone")
-    list_filter = ("created_at",)
+admin.site.register(Property, PropertyAdmin)
