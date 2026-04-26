7cf7a024f5b45e16600536e7fc4c27973e3e889a
BowlingLegacy <bowlinglegacyllc@outlook.com>
2026-04-26 06:59:35 -0700

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path("", include("main.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve uploaded media files (images)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
