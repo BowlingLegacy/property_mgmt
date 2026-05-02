4800f4d980a537cf0bdd8794ce56d8454621ade2
BowlingLegacy <bowlinglegacyllc@outlook.com>
2026-05-01 18:58:49 -0700

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -5,7 +5,7 @@ from .models import (
     HousingApplication,
     ApplicantDocument,
     BlogPost,
-    BlogComment
+    BlogComment,
 )
 
 
@@ -17,17 +17,148 @@ class PropertyImageInline(admin.TabularInline):
 @admin.register(Property)
 class PropertyAdmin(admin.ModelAdmin):
     inlines = [PropertyImageInline]
+    list_display = ("name", "availability_status", "available_date")
 
 
 class ApplicantDocumentInline(admin.TabularInline):
     model = ApplicantDocument
     extra = 0
+    can_delete = False
+
+    fields = (
+        "name",
+        "document_type",
+        "file",
+        "status",
+        "needs_signature",
+        "needs_initials",
+        "signed_at",
+        "submitted_at",
+        "locked",
+        "created_at",
+    )
+
+    readonly_fields = (
+        "locked",
+        "created_at",
+    )
 
 
 @admin.register(HousingApplication)
 class HousingApplicationAdmin(admin.ModelAdmin):
     inlines = [ApplicantDocumentInline]
-    readonly_fields = ("created_at",)
+
+    list_display = (
+        "full_name",
+        "property",
+        "email",
+        "phone",
+        "created_at",
+    )
+
+    search_fields = (
+        "full_name",
+        "email",
+        "phone",
+    )
+
+    list_filter = (
+        "property",
+        "created_at",
+    )
+
+    readonly_fields = (
+        "created_at",
+    )
+
+    fieldsets = (
+        ("Resident Profile", {
+            "fields": (
+                "property",
+                "full_name",
+                "phone",
+                "email",
+                "age",
+            )
+        }),
+        ("Current Address", {
+            "fields": (
+                "current_address",
+                "current_address_length",
+            )
+        }),
+        ("Previous Addresses", {
+            "fields": (
+                "previous_address_1",
+                "previous_address_1_length",
+                "previous_address_2",
+                "previous_address_2_length",
+                "previous_address_3",
+                "previous_address_3_length",
+            )
+        }),
+        ("Identification", {
+            "fields": (
+                "drivers_license_number",
+                "has_valid_odl",
+                "oregon_id_number",
+                "id_upload",
+            )
+        }),
+        ("Income / Employment", {
+            "fields": (
+                "income_source",
+                "monthly_income",
+                "employer_name",
+                "employment_length",
+            )
+        }),
+        ("Housing / Recovery / Background", {
+            "fields": (
+                "housing_need",
+                "previous_evictions",
+                "in_recovery",
+                "drug_of_choice",
+                "on_parole",
+                "parole_officer_name",
+                "parole_officer_phone",
+                "felony_history",
+                "odoc_time_served",
+            )
+        }),
+        ("References", {
+            "fields": (
+                "reference_1_name",
+                "reference_1_phone",
+                "reference_1_relationship",
+                "reference_1_type",
+                "reference_2_name",
+                "reference_2_phone",
+                "reference_2_relationship",
+                "reference_2_type",
+            )
+        }),
+        ("Acknowledgments", {
+            "fields": (
+                "sobriety_acknowledgment",
+                "unconditional_regard_acknowledgment",
+            )
+        }),
+        ("Notes / System", {
+            "fields": (
+                "additional_notes",
+                "created_at",
+            )
+        }),
+    )
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return [field.name for field in self.model._meta.fields]
+        return self.readonly_fields
 
 
 @admin.register(BlogPost)
@@ -39,7 +170,8 @@ class BlogPostAdmin(admin.ModelAdmin):
 @admin.register(BlogComment)
 class BlogCommentAdmin(admin.ModelAdmin):
     list_display = ("name", "post", "approved", "created_at")
-    list_filter = ("approved",)
+    list_filter = ("approved", "created_at")
+    search_fields = ("name", "email", "comment")
     actions = ["approve_comments"]
 
     def approve_comments(self, request, queryset):
