6a7141a0b6b2c558c8ab50eea418a5898e617ccf
BowlingLegacy <bowlinglegacyllc@outlook.com>
2026-05-05 15:47:37 -0700

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
 
     path("signup/", views.signup, name="signup"),
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(next_page="home"), name="logout"),
+    path("logout/", views.logout_view, name="logout"),
 
     path("tenant-dashboard/", views.tenant_dashboard, name="tenant_dashboard"),
     path("landlord-dashboard/", views.landlord_dashboard, name="landlord_dashboard"),
