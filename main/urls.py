0f5c2ceb4839f9750908e1f1b9b6e7575ad1495f
BowlingLegacy <bowlinglegacyllc@outlook.com>
2026-05-14 09:23:41 -0700

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -14,6 +14,27 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", views.logout_view, name="logout"),
 
+    path("password-reset/", auth_views.PasswordResetView.as_view(
+    template_name="password_reset.html",
+    email_template_name="password_reset_email.html",
+    subject_template_name="password_reset_subject.txt",
+    success_url="/password-reset/done/"
+),  name="password_reset"),
+
+    path("password-reset/done/", auth_views.PasswordResetDoneView.as_view(
+    template_name="password_reset_done.html"
+),  name="password_reset_done"),
+
+    path("password-reset-confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(
+    template_name="password_reset_confirm.html",
+    success_url="/password-reset/complete/"
+),  name="password_reset_confirm"),
+
+    path("password-reset/complete/", auth_views.PasswordResetCompleteView.as_view(
+    template_name="password_reset_complete.html"
+),  name="password_reset_complete"),
+    
+
     path("tenant-dashboard/", views.tenant_dashboard, name="tenant_dashboard"),
     path("landlord-dashboard/", views.landlord_dashboard, name="landlord_dashboard"),
     path("landlord-message/<int:message_id>/", views.landlord_message_detail, name="landlord_message_detail"),
