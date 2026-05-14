fe7f055720cae7cc9dbf7648fe71e9be2dd2c9d5
BowlingLegacy <bowlinglegacyllc@outlook.com>
2026-05-14 11:58:42 -0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -126,7 +126,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 # EMAIL SETTINGS
 # ---------------------------------------------------------
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "smtp.office365.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
