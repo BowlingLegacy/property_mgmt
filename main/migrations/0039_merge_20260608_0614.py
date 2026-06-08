77ddded32c51613e87c8980392c5af7b0c05d868
BowlingLegacy <bowlinglegacyllc@outlook.com>
2026-06-07 23:20:09 -0700

    Merge main app migrations

---
+++ main/migrations/0039_merge_20260608_0614.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("main", "0038_alter_existingresidentintake_application"),
+        ("main", "0038_sms_staff_copy_logs"),
+    ]
+
+    operations = []
